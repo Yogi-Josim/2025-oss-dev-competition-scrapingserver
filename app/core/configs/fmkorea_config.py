1ec3517d821a57c6d83bca5a4732063cf1f7b639
jae woo Jung <awfjol2008@gmail.com>
2025-08-18 05:01:15 +0900

    fix: adjust indentation and add listStyle to fmkorea scraping configuration

--- app/core/configs/fmkorea_config.py
+++ app/core/configs/fmkorea_config.py
@@ -2,23 +2,25 @@ from pydantic import BaseModel
 from typing import List, Dict
 
 class FmkoreaSettings(BaseModel):
-    BASE_URL: str = "https://www.fmkorea.com"
+  BASE_URL: str = "https://www.fmkorea.com"
 
-    BOARDS_TO_SCRAPE: List[Dict[str, str]] = [
-        {
-            "id": "humor",
-            "category": "1899663",
-            "order_type": "desc",
-            "name": "유머/이슈 게시판"
-        },
-    ]
+  BOARDS_TO_SCRAPE: List[Dict[str, str]] = [
+    {
+      "id": "humor",
+      "category": "1899663",
+      "order_type": "desc",
+      "listStyle": "webzine",
+      "name": "유머/이슈 게시판",
+    },
+  ]
 
-    POST_ROW_SELECTOR: str = "ul.bd_lst li.li"
-    POST_LINK_SELECTOR: str = "a.hx"
-    TITLE_SELECTOR: str = "h1.np_18px"
-    CONTENT_SELECTOR: str = "article > div.xe_content"
-    TIME_SELECTOR: str = "span.time"
-    AUTHOR_SELECTOR: str = "span.by"
-    COMMENT_SELECTOR: str = "span.comment"
+  POST_ROW_SELECTOR: str = "ul.bd_lst li.li"
+  POST_LINK_SELECTOR: str = "a.hx"
+  TITLE_SELECTOR: str = "h1.np_18px"
+  CONTENT_SELECTOR: str = "article > div.xe_content"
+  TIME_SELECTOR: str = "span.time"
+  AUTHOR_SELECTOR: str = "span.by"
+  COMMENT_SELECTOR: str = "span.comment"
+
 
 fmkorea_settings = FmkoreaSettings()
